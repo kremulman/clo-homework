20f19532e459c27e4f87521e5b97deee80a39e43
kremulman <kremulman@gmail.com>
2024-01-09 23:35:22 +0900

    기본파일 커밋

---
+++ src/main/java/com/sangcheol/clohomework/ClohomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sangcheol.clohomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClohomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClohomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sangcheol/clohomework/ClohomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangcheol.clohomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClohomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
